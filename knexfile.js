2b2c9b6f603d4b20b5ce8cd5129b24a90be904b1
bekabear <bekatracurso@gmail.com>
2025-08-28 14:47:14 -0300

    Configuração do banco de dados

---
+++ knexfile.js
@@ -0,0 +1,29 @@
+// Update with your config settings.
+const path = require("node:path")
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './src/database/app.db'
+  },
+  useNullAsDefault: true,
+    migrations: {
+      directory: path.join("./src/database/migrations")
+    }
+  },
+  production: {
+    client: 'pg',
+    connection: {
+      connectionString: process.env.PROD_DB,
+      sll: {rejectUnauthorized: false }
+    },
+    migrations: {
+      directory: path.join("./src/database/migrations")
+    }
+  }
+
+};
---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const path = require("node:path");
+const router = require("./src/routes/index.js");
+const dotenv = require("dotenv");
+
+dotenv.config();
+const PORT = process.env.PORT || 3333
+const app = express()
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "src/routes")));
+app.use("/", router);
+app.listen(PORT, () => {
+    console.log("Servidor rodando na porta " + PORT);
+})
