23cc49ffd45ace58d058a0aed59d5b7992574739
bekabear <bekatracurso@gmail.com>
2025-09-01 14:50:50 -0300

    Ajuste no caminho das rotas

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const path = require("path");
-const router = require("./src/routes/index.js")
+const router = require("./src/routes")
 const dotenv = require("dotenv")
 
 dotenv.config();
